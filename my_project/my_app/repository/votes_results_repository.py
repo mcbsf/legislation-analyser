6f89215d8358b48e9968a2761cb9014b6eec575e
mariocbsf <mariocbsf@gmail.com>
2024-03-14 16:05:24 +0000

    removing unused design pattern

--- my_project/my_app/repository/votes_results_repository.py
+++
@@ -1,7 +0,0 @@
-import csv
-from my_app.models import VoteResult
-from my_app.parsers.votes_results_parser import VotesResultsParser
-
-class VotesResultsRepository:
-    def __init__(self, path):
-        self.votes_results = VotesResultsParser().parse_csv(path)
